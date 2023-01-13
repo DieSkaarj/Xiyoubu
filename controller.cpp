51a1369c78b0b785eac5158d39e6ffbb8ca6248a
David 'Erosion' Oberlin <40402624+DieSkaarj@users.noreply.github.com>
2023-01-13 13:48:06 +0000

    Add files via upload
    Fixed debounce on controller.

--- controller.cpp
+++ controller.cpp
@@ -7,7 +7,8 @@
 #define CONTROLLER PORTD
 #define CONTROLLER_READ PIND
 #define CONTROLLER_DDR DDRD
-#define CLEAR B00000000
+#define CONTROLLER_CONF 0b00000000
+#define CONTROLLER_INIT 0b00000000
 #define SELECT PIND2
 #define BUTTON_HOLD 500U
 
@@ -21,14 +22,14 @@ Controller::Controller(Console &t_console):
   console(t_console)
 {
   /*
-   *  Set Port D to read and clear pull-up resistors 
+   *  Set Port D to input and clear pull-up resistors 
    */
-  CONTROLLER_DDR = CLEAR;
-  CONTROLLER = CLEAR;
+  CONTROLLER_DDR = CONTROLLER_CONF;
+  CONTROLLER = CONTROLLER_INIT;
   /*
    * Set interrupt mode to logical CHANGE on INT0 (pin18/D2.)
    */
-  EICRA = 0x01;
+  EICRA = _BV(ISC00);
   EIMSK = _BV(INT0);
 
   sei();      
@@ -45,9 +46,8 @@ void Controller::poll()
   /*
    * Is SELECT signal high or low?
    */
-  const bool    signal{(sig_mask&CONTROLLER_READ)>>SELECT ? true : false };
-  const uint8_t buttons{pad_mask|CONTROLLER_READ};
-
+  const bool    signal{(SIG_MASK&CONTROLLER_READ)>>SELECT ? true : false };
+  const uint8_t buttons{PAD_MASK|CONTROLLER_READ};
   /*
    * The controllers' port is an 8Bit register. To only manipulate the HIGH and LOW
    * bits a 255 (0xFF) mask is applied and inverted when necessary.
@@ -59,31 +59,22 @@ void Controller::poll()
   (~buttons<<8)&(~0xff) : (buttons^0xff);
 }
 
-void Controller::handle(const uint32_t delta)
+void Controller::handle(const uint32_t t_ticks)
 {
  /*
    * If lines connected to Left and Right are low the console asserts that a
    * 3BTN controller is present. These values are masked when handling,
    * as only the button presses are relevant here.
    */
-  const uint16_t  status{ (_on_read&~(PAD3)) };
   static uint16_t last_read{ 0 };
-  static uint32_t debounce{ 0 };
-
-  /*
-   * Reset time delta if no buttons are pressed
-   */
+  static uint32_t delta{ BUTTON_HOLD };
+  const uint16_t  status{ ( _on_read&(  ~PAD3 ) ) };
+  uint32_t debounce{ t_ticks-delta };
 
   if
-  ( (status!=last_read ) )
-  {
-    debounce=delta;
-  }
-
-  last_read=status;
-
-  if
-  ( ( delta - debounce < BUTTON_HOLD ) )
+  ( last_read!=status || debounce > BUTTON_HOLD)
+    delta=t_ticks;
+  else
     return;
 
   switch
@@ -102,5 +93,5 @@ void Controller::handle(const uint32_t delta)
     break;
   }
 
-  debounce=delta;
+  last_read=status;
 }
